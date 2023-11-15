2c6a42012565c4cd0e461f68bdf95edad3e8a7a6
Prince Kanu <kanuprince93@yahoo.com>
2023-11-15 12:44:46 -0600

    added more to console log of db connection and changes title in homepage

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@princecluster.olnk3yw.mongodb.net/${process.env.MONGODB_DATABASE_NAME}?retryWrites=true&w=majority`)
-        console.log('MongoDB Connected');
+        console.log(`MongoDB Connect: ${conn.connection.host}`);
     } catch (err) {
         console.log(err)
         process.exit(1)
