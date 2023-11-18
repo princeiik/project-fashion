c5d2ee39414e29fbc0c1edf3c3d7a153176ad3c9
Prince Kanu <kanuprince93@yahoo.com>
2023-11-17 19:55:53 -0600

    moved routers to different folder and connect routers to server js file

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ require('dotenv').config({path: './config/.env'});
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors')
-const Item = require('./models/Item')
 const connectDB = require('./config/database')
 
 const app = express()
 
 const port = process.env.PORT || 4000
+
+const {ItemRouter} = require('./routers')
 
 connectDB()
 
@@ -22,6 +23,7 @@ app.use(express.static('public'))
 //Parse requests
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
+app.use('/item', ItemRouter)
 
 //Set EJS as templating engine
 app.set('view engine','ejs')
@@ -31,46 +33,10 @@ app.get('/',(req, res) => {
     res.render('index')
 })
 
-app.get('/item', async (req, res) => {
-    const items = await Item.find({})
-    res.render('item', {items})
-})
 
-//Create
-app.post('/item', async (req, res) => {
-    const newItem = new Item(req.body)
-    try {
-        await newItem.save()
-        res.redirect('/item')
-    } catch(err) {
-        res.redirect('/item?error=true')
-    }
-})
-
-//Update
-app.post('/item/update/:id', async (req, res) => {
-    const {id} = req.params
-    const {name, description} = req.body
-    try {
-        await Item.findByIdAndUpdate(id, {name, description})
-        res.redirect('/item')
-    } catch(err) {
-        res.redirect('/item?error=true')
-    }
-})
-
-//Delete
-app.delete('/item/delete/:id', async (req, res) => {
-    const {id} = req.params
-    try {
-        await Item.findByIdAndDelete(id)
-        res.status(200).json({ message: 'Item deleted successfully' })
-    } catch(err) {
-        res.redirect('/item?error=true')
-    }
-})
 
 //Start the Server
+//npm run dev
 app.listen(port, () => {
     console.log(`Server running on: http://localhost:${port}`)
 })
