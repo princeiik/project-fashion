7393c2fc4dfc7d78d98e1963a0b3f9337568ba9a
Prince Kanu <kanuprince93@yahoo.com>
2023-11-20 22:05:27 -0600

    deleted some extra code in server js

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ const port = process.env.PORT || 4000
 
 connectDB()
 
-
 //enable CORS
 app.use(cors())
 
@@ -29,49 +28,6 @@ app.set('view engine','ejs')
 //Routes
 app.use('/', mainRoutes)
 app.use('/item', itemRoutes)
-
-// app.get('/',(req, res) => {
-//     res.render('index')
-// })
-
-// app.get('/item', async (req, res) => {
-//     const items = await Item.find({})
-//     res.render('item', {items})
-// })
-
-// //Create
-// app.post('/item', async (req, res) => {
-//     const newItem = new Item(req.body)
-//     try {
-//         await newItem.save()
-//         res.redirect('/item')
-//     } catch(err) {
-//         res.redirect('/item?error=true')
-//     }
-// })
-
-// //Update
-// app.post('/item/update/:id', async (req, res) => {
-//     const {id} = req.params
-//     const {name, description} = req.body
-//     try {
-//         await Item.findByIdAndUpdate(id, {name, description})
-//         res.redirect('/item')
-//     } catch(err) {
-//         res.redirect('/item?error=true')
-//     }
-// })
-
-// //Delete
-// app.delete('/item/delete/:id', async (req, res) => {
-//     const {id} = req.params
-//     try {
-//         await Item.findByIdAndDelete(id)
-//         res.status(200).json({ message: 'Item deleted successfully' })
-//     } catch(err) {
-//         res.redirect('/item?error=true')
-//     }
-// })
 
 //Start the Server
 //npm run dev
