4192eef394e5bcad8af4cc455262cd916964be01
Prince Kanu <kanuprince93@yahoo.com>
2024-01-06 11:39:13 -0600

    added passport authentication for server.js

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const cors = require('cors')
 const connectDB = require('./config/database')
 const mainRoutes = require('./routes/main')
 const postRoutes = require('./routes/post')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const User = require('./models/User')
 
 const app = express()
 
@@ -24,6 +27,30 @@ app.use(express.json())
 
 //Set EJS as templating engine
 app.set('view engine','ejs')
+
+// Setup Sessions - stored in MongoDB
+app.use(session({
+    secret: 'This is Project Fashion',
+    resave: false,
+    saveUninitialized: false
+}))
+
+//Turn on passport and session
+app.use(passport.initialize())
+app.use(passport.session())
+
+//Store authentication in User model
+passport.use(new LocalStrategy(User.authenticate()))
+
+//Scramble the password and unscramble the password
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
+//passing current user info to all routes
+app.use((req,res,next) => {
+    res.locals.currentUser = req.user
+    next()
+})
 
 //Routes
 app.use('/', mainRoutes)
