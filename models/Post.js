6f3b318a66fe2289a0b94010c707dc7e819e57e3
Prince Kanu <kanuprince93@yahoo.com>
2024-01-14 13:20:56 -0600

    added createdBy schema and linked it with User model

--- models/Post.js
+++ models/Post.js
@@ -17,6 +17,10 @@ const PostSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
     createdAt: {
         type: Date,
         default: Date.now
