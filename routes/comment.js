f8f1c862dae6b9ce55b39040be6ebc8188d906fa
Prince Kanu <kanuprince93@yahoo.com>
2024-02-15 21:32:03 -0600

    added comments for comment route

--- routes/comment.js
+++ routes/comment.js
@@ -1,6 +1,11 @@
+//Handles GET request
+//Handles POST method request for adding a new task
+
 const express = require('express')
 const router = express.Router()
 const commentController = require('../controllers/comment')
+
+//Add specific routes for specific tasks
 
 router.post('/createComment/:id', commentController.createComment)
 router.delete('/deleteComment/:id', commentController.deleteComment)
