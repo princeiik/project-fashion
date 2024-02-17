5505a59df0d61391523083c09d90114eab76304f
Prince Kanu <kanuprince93@yahoo.com>
2024-02-17 02:55:56 -0600

    added comments to post route

--- routes/post.js
+++ routes/post.js
@@ -1,8 +1,12 @@
+//Handles GET request
+//Handles POST method request for adding a new task
+
 const express = require('express')
 const router = express.Router()
 const postController = require('../controllers/post')
 const upload = require("../middleware/multer");
 
+//Add specific routes for specific tasks
 
 router.get('/:id', postController.getPost)
 router.post('/', upload.single("file"), postController.createPost)
